08ee782f00f7c73894d17fd351a5374c05328f98
Chandankumar Renuka <chandankumarrr@infinite.com>
2023-09-05 19:44:18 +0530

    Csharp

---
+++ Csharp/Day2_DotNet/Day2_DotNet/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_DotNet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your First Name :");
+            string fname = Console.ReadLine();
+            //Console.WriteLine("Your Name is " + fname); // Concatenation
+            //Console.WriteLine("Your Name is {0}", fname);//placeholder
+            //Multiple Placeholder example to display data
+            Console.WriteLine("Enter your Last Name:");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Your First Name is :{0} and your Last name is :{1}", fname, lname);
+            //string interpolation to display data
+            Console.WriteLine($"Your FirstName is {fname} and your lastName is {lname}");
+            Myclass.MyFunction();
+
+            //  func2();
+            Console.Read();
+        }
+
+        static void func2()
+        {
+            Console.WriteLine("This func2");
+            Console.WriteLine("Enter your Age :");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You are {age} years old");
+            Console.WriteLine("Min Value = {0}", int.MinValue);
+            Console.WriteLine("Min Value = {0}", int.MaxValue);
+        }
+    }
+
+    class Myclass
+    {
+        public static void MyFunction()
+        {
+            Console.WriteLine("We are discussing Main Function");
+            int num = 30;
+            bool b;
+            /* if(num==10)
+             {
+                 b = true;
+             }
+             else
+             {
+                 b = false; 
+             }
+             Console.WriteLine("Number == 10 is {0}", b); */
+
+            //the above lines of code can be replaced with a ternary operator as below
+            b = num == 10 ? true : false;
+
+            Console.WriteLine("Number == 10 is {0}", b);
+        }
+    }
+}
---
+++ Csharp/Day2_DotNet/Day2_DotNet/TypeConversion.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_DotNet
+{
+    class TypeConversion
+    {
+        static void Main()
+        {
+            Console.WriteLine("Understanding Type Conversions..");
+            int i = 100;
+            float f = i;   //implicit conversion
+            Console.WriteLine(f);
+            i = (int)f;  //explicit type casting 
+            Console.WriteLine(i);
+
+            float f2 = 12434545678966.44356f;
+            //  i=Convert.ToInt32(f2);  //explicit typecasting using conversion function of C#
+            i = (int)f2;
+
+            //try increasing the value of the float variable and observe the below:
+
+            //conversion function is throwing an exception, where as typecasting
+
+            // is showing the limitation
+            Console.WriteLine(i);
+
+
+
+            string str = "100";
+            //i = int.Parse(str);
+            //Console.WriteLine(i);
+
+            //now lets use tryparse() to avoid exception unlike the above parse()
+            //even if it fails to convert
+
+            int result;
+            bool success = int.TryParse(str, out result);
+
+            if (success)
+            {
+                Console.WriteLine("The number is " + result);
+            }
+            else
+                Console.WriteLine("Invalid Data");
+            Console.WriteLine("-----------------");
+            RefConversion();
+            Console.Read();
+        }
+
+        static void RefConversion()
+        {
+            int i = 10;
+            object o;
+            o = i;  //value type to reference type  -- boxing
+            Console.WriteLine(o);
+            float salary;
+            Console.WriteLine("Enter Salary :");
+            //  salary = float.Parse(Console.ReadLine());  //ref type to value type -- unboxing
+            salary = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(salary);
+            string s = "aaa";
+            o = s;  // implicit casting between 2 reference types
+            Console.WriteLine(o);
+            Console.WriteLine(s);
+        }
+    }
+}
+
+
+
