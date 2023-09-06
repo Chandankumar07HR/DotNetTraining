3158519d75d398736ce6bcf899486338c3938d4c
Chandankumar Renuka <chandankumarrr@infinite.com>
2023-09-07 01:01:31 +0530

    assignment

--- Assignment/Assignment1/Assignment1/Program.cs
+++ Assignment/Assignment1/Assignment1/Program.cs
@@ -27,10 +27,11 @@ namespace Assignment1
             else
                 Console.WriteLine($"{num1} and {num2} are not equal");
             Program.negative(); //calling nagative() function in main function and the function negative is static so i dont want to create object and the negative function is in same class as of main.
+            Program.operations();
             Console.ReadLine();
 
         }
-    //2. Write a C# Sharp program to check whether a given number is positive or negative.
+ //2. Write a C# Sharp program to check whether a given number is positive or negative.
         static void negative()
         {
             int number = 14;
@@ -41,6 +42,66 @@ namespace Assignment1
             else if(number < 0)
             {
                 Console.WriteLine($"{number} is a negative number");
+            }
+        }
+//3. Write a C# Sharp program that takes two numbers as input and performs all operations (+,-,*,/) on them and displays the result of that operation. 
+        /*Test Data
+        Input first number: 20
+        Input operation: -  
+        Input second number: 12
+        Expected Output :     
+         20 - 12 = 8  */
+        static void operations()
+        {
+            Console.Write("Enter the first number: ");
+            if (double.TryParse(Console.ReadLine(), out double num1))
+            {
+                Console.Write("Enter the operation (+, -, *, /): ");
+                char operation = Console.ReadKey().KeyChar;
+                Console.WriteLine(); // Move to the next line
+
+                Console.Write("Enter the second number: ");
+                if (double.TryParse(Console.ReadLine(), out double num2))
+                {
+                    double result = 0.0;
+
+                    switch (operation)
+                    {
+                        case '+':
+                            result = num1 + num2;
+                            break;
+                        case '-':
+                            result = num1 - num2;
+                            break;
+                        case '*':
+                            result = num1 * num2;
+                            break;
+                        case '/':
+                            if (num2 != 0)
+                            {
+                                result = num1 / num2;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: Division by zero is not allowed.");
+                                return;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Invalid operation.");
+                            return;
+                    }
+
+                    Console.WriteLine($"{num1} {operation} {num2} = {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid second number input.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid first number input.");
             }
         }
 
